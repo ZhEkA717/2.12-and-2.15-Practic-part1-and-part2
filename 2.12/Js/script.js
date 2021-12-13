e46514534688048589689a6e4e28365be5433c90
Zheka <grushevskiy.yevgeniy@mail.ru>
2021-12-13 14:58:51 +0300

    sfasf"f

--- 2.12/Js/script.js
+++ 2.12/Js/script.js
@@ -1,19 +1,19 @@
-let numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', "");
+// let numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', "");
 
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
 
-let a1 = prompt('Один из последних просмотренных фильмов?', "");
-let a2 = prompt('Один из последних просмотренных фильмов?', "");
-let a3 = prompt('На сколько оцените его?', "");
-let a4 = prompt('На сколько оцените его?', "");
+// let a1 = prompt('Один из последних просмотренных фильмов?', "");
+// let a2 = prompt('Один из последних просмотренных фильмов?', "");
+// let a3 = prompt('На сколько оцените его?', "");
+// let a4 = prompt('На сколько оцените его?', "");
 
-personalMovieDB.movies[a1]=a3;
-personalMovieDB.movies[a2]=a4;
+// personalMovieDB.movies[a1]=a3;
+// personalMovieDB.movies[a2]=a4;
 
-console.log(personalMovieDB.movies);
+// console.log(personalMovieDB.movies);
